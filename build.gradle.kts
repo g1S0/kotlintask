32ecfaa4090f9231161b3875fa1b65849732dc49
ddasfasdfs <glibsoldatenko@gmail.com>
2024-09-17 22:46:50 +0300

    feat: add libraries for task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    id("io.ktor.plugin") version "2.3.5"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
 }
 
 group = "org.example"
@@ -11,6 +13,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:2.3.0")
+    implementation("io.ktor:ktor-client-cio:2.3.0")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
 }
 
 tasks.test {
